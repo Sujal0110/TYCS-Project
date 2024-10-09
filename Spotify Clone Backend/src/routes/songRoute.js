31671281408b75269ea28901d7d494ea47e0fbc7
Sujal0110 <sujalkesarkar0@gmail.com>
2024-10-09 22:27:25 +0530

    complete backend

--- Spotify Clone Backend/src/routes/songRoute.js
+++ Spotify Clone Backend/src/routes/songRoute.js
@@ -1,9 +1,22 @@
-import { addSong, listenSong } from "../controllers/songController";
+import {
+    addSong,
+    listenSong,
+    removeSong,
+} from "../controllers/songController.js";
 import express from "express";
+import upload from "../middleware/multer.js";
 
 const songRouter = express.Router();
 
-songRouter.post("/add", addSong);
+songRouter.post(
+    "/add",
+    upload.fields(
+        { name: "image", maxCount: 1 },
+        { name: "audio", maxCount: 1 }
+    ),
+    addSong
+);
 songRouter.get("/list", listenSong);
+songRouter.post("/remove", removeSong);
 
 export default songRouter;
